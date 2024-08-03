7ed5bbc820055f8fc8671165f5970b3bcf276dca
jowisaidman <joelsaidman1@gmail.com>
2024-08-03 09:15:37 -0300

    styles details

--- CryptoOnTheFlyFE/screens/ScanQRScreen.js
+++ CryptoOnTheFlyFE/screens/ScanQRScreen.js
@@ -23,6 +23,10 @@ export default function ScanQRScreen() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
+  function scanningQR() {
+    console.log("Scanning QR!");
+  }
+
   return (
     <View style={styles.container}>
       <CameraView style={styles.camera} facing={facing}>
@@ -32,36 +36,50 @@ export default function ScanQRScreen() {
           </TouchableOpacity>
         </View>
       </CameraView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={scanningQR}>
+            <Text style={styles.text}>Scan next QR</Text>
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  message: {
-    textAlign: 'center',
-    paddingBottom: 10,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
-  },
-  button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-});
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    message: {
+      textAlign: 'center',
+      paddingBottom: 10,
+    },
+    camera: {
+      width: "80%",
+      height: "50%",
+      borderRadius: 10,
+      overflow: 'hidden',
+    },
+    buttonContainer: {
+      position: 'absolute',
+      bottom: 50,
+      flexDirection: 'row',
+      width: '100%',
+      justifyContent: 'center',
+      backgroundColor: 'transparent',
+    },
+    button: {
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+      padding: 10,
+      borderRadius: 5,
+    },
+    text: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: 'white',
+    },
+  });
+  
