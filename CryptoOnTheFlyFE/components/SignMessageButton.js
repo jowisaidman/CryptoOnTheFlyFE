43c32345839049a01df40a6947a9263ff914858a
jowisaidman <joelsaidman1@gmail.com>
2024-08-03 17:31:43 -0300

    add loading screen for signing message

--- CryptoOnTheFlyFE/components/SignMessageButton.js
+++ CryptoOnTheFlyFE/components/SignMessageButton.js
@@ -1,23 +1,24 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Button } from 'react-native';
 import { getValueFor } from '../utils/SecureStorage';
 import { signMessage } from '../utils/WalletService';
 
-const SignMessageButton = ({message, navigation}) => {
-  
-  const handleSigningMessage = async () => {
-    let privateKey = await getValueFor("seedPhrase");
-    let signedMessage = await signMessage(message, privateKey);
-    console.log("signedMessage", signedMessage);
-    navigation.navigate("SignedMessageQR", {signedMessage})
-  }
+const SignMessageButton = ({message, navigation, setIsLoading}) => {
+    const handleSigningMessage = async () => {
+      setIsLoading(true);
+      let privateKey = await getValueFor("seedPhrase");
+      let signedMessage = await signMessage(message, privateKey);
+      console.log("signedMessage", signedMessage);
+      setIsLoading(false);
+      navigation.navigate("SignedMessageQR", {signedMessage})
+    }
 
-  return (
-    <Button 
-        title="Sign message"
-        onPress={() => handleSigningMessage()}
-    />
-  );
+    return (
+        <Button 
+            title="Sign message"
+            onPress={() => handleSigningMessage()}
+        />
+    );
 };
 
 export default SignMessageButton;
--- CryptoOnTheFlyFE/screens/SignMessageScreen.js
+++ CryptoOnTheFlyFE/screens/SignMessageScreen.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import SignMessageButton from '../components/SignMessageButton';
+import Loading from '../components/Loader';
 
 const SignMessageScreen = ({ route, navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Sign message: {route.params.messageInfo}</Text>
-      <SignMessageButton message={route.params.messageInfo} navigation={navigation}/>
-    </View>
-  );
+    const [isLoading, setIsLoading] = useState(false);
+
+    if (isLoading) {
+        return <Loading text="Signing message ..."/>
+    } else {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>Sign message: {route.params.messageInfo}</Text>
+                <SignMessageButton message={route.params.messageInfo} navigation={navigation} setIsLoading={setIsLoading}/>
+            </View>
+        );
+    }
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 24,
+        marginBottom: 20,
+    },
 });
 
 export default SignMessageScreen;
