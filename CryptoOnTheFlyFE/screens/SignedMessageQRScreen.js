4afa7f65e42f4839e35f122f13878151553f4ae5
jowisaidman <joelsaidman1@gmail.com>
2024-08-03 21:27:34 -0300

    styles for show QR signed message

--- CryptoOnTheFlyFE/screens/SignedMessageQRScreen.js
+++ CryptoOnTheFlyFE/screens/SignedMessageQRScreen.js
@@ -4,25 +4,28 @@ import { View, Text, StyleSheet, Button } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 
 const SignedMessageQRScreen = ({ route, navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>This is your signed message, scan it from your plugin</Text>
-      <QRCode value={route.params.signedMessage} size={300} />
-      <Button title="Go to home" onPress={() => navigation.navigate('Home')}/>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>This is your signed message, scan it from your plugin</Text>
+            <QRCode value={route.params.signedMessage} size={300}/>
+            <Button title="Go to home" onPress={() => navigation.navigate('Home')}/>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 16,
-    marginBottom: 100,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#1A1733',
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#FDFEFF',
+        marginBottom: '5%',
+    },
 });
 
 export default SignedMessageQRScreen;
